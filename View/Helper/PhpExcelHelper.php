baf63d0b9ccf80467782f01fd59b32d784d4203f
Mutlu Tevfik Koçak <mtkocak@gmail.com>
2013-07-17 11:02:28 +0300

    Undefined variable: offset error

--- View/Helper/PhpExcelHelper.php
+++ View/Helper/PhpExcelHelper.php
@@ -79,6 +79,7 @@ class PhpExcelHelper extends AppHelper {
 	 */
 	public function addTableHeader($data, $params = array()) {
 		// offset
+		$offset = 0;
 		if (array_key_exists('offset', $params))
 			$offset = is_numeric($params['offset']) ? (int)$params['offset'] : PHPExcel_Cell::columnIndexFromString($params['offset']);
 		// font name
