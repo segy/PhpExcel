fb5611e21830035ab965b418c7f7e45735afc855
Tomas Saghy <segy@riesenia.com>
2013-12-01 17:35:48 +0100

    Changed formatting, some methods and comments. Added Vendor.

---
+++ View/Helper/PhpExcelAppHelper.php
@@ -0,0 +1,12 @@
+<?php
+App::uses('AppHelper', 'View/Helper');
+
+/**
+ * PhpExcel AppHelper.
+ *
+ * @package PhpExcel
+ * @author segy
+ */
+class PhpExcelAppHelper extends AppHelper {
+
+}
--- View/Helper/PhpExcelHelper.php
+++ View/Helper/PhpExcelHelper.php
@@ -1,216 +1,288 @@
 <?php
-App::uses('AppHelper', 'Helper');
+App::uses('PhpExcelAppHelper', 'PhpExcel.View/Helper');
 
 /**
  * Helper for working with PHPExcel class.
- * PHPExcel has to be in the vendors directory.
+ *
+ * @package PhpExcel
+ * @author segy
  */
+class PhpExcelHelper extends PhpExcelAppHelper {
+    /**
+     * Instance of PHPExcel class
+     *
+     * @var PHPExcel
+     */
+    protected $_xls;
 
-class PhpExcelHelper extends AppHelper {
-	/**
-	 * Instance of PHPExcel class
-	 * @var object
-	 */
-	public $xls;
-	/**
-	 * Pointer to actual row
-	 * @var int
-	 */
-	protected $row = 1;
-	/**
-	 * Internal table params 
-	 * @var array
-	 */
-	protected $tableParams;
-	
-	/**
-	 * Constructor
-	 */
-	public function __construct(View $view, $settings = array()) {
-        parent::__construct($view, $settings);
+    /**
+     * Pointer to current row
+     *
+     * @var int
+     */
+    protected $_row = 1;
+
+    /**
+     * Internal table params
+     *
+     * @var array
+     */
+    protected $_tableParams;
+
+    /**
+     * Create new worksheet or load it from existing file
+     *
+     * @return $this for method chaining
+     */
+    public function createWorksheet() {
+        // load vendor classes
+        App::import('Vendor', 'PhpExcel.PHPExcel');
+
+        $this->_xls = new PHPExcel();
+        $this->_row = 1;
+
+        return $this;
     }
-	
-	/**
-	 * Create new worksheet
-	 */
-	public function createWorksheet() {
-		$this->loadEssentials();
-		$this->xls = new PHPExcel();
-	}
-	
-	/**
-	 * Create new worksheet from existing file
-	 */
-	public function loadWorksheet($path) {
-		$this->loadEssentials();
-		$this->xls = PHPExcel_IOFactory::load($path);
-	}
-	
-	/**
-	 * Set row pointer
-	 */
-	public function setRow($to) {
-		$this->row = (int)$to;
-	}
-	
-	/**
-	 * Set default font
-	 */
-	public function setDefaultFont($name, $size) {
-		$this->xls->getDefaultStyle()->getFont()->setName($name);
-		$this->xls->getDefaultStyle()->getFont()->setSize($size);
-	}
-	
-	/**
-	 * Start table
-	 * inserts table header and sets table params
-	 * Possible keys for data:
-	 * 	label 	-	table heading
-	 * 	width	-	"auto" or units
-	 * 	filter	-	true to set excel filter for column
-	 * 	wrap	-	true to wrap text in column
-	 * Possible keys for params:
-	 * 	offset	-	column offset (numeric or text)
-	 * 	font	-	font name
-	 * 	size	-	font size
-	 * 	bold	-	true for bold text
-	 * 	italic	-	true for italic text
-	 * 	
-	 */
-	public function addTableHeader($data, $params = array()) {
-		
-		// offset
-		$offset = 0;
-		
-		// offset
-		$offset = 0;
-		if (array_key_exists('offset', $params))
-			$offset = is_numeric($params['offset']) ? (int)$params['offset'] : PHPExcel_Cell::columnIndexFromString($params['offset']);
-		// font name
-		if (array_key_exists('font', $params))
-			$this->xls->getActiveSheet()->getStyle($this->row)->getFont()->setName($params['font_name']);
-		// font size
-		if (array_key_exists('size', $params))
-			$this->xls->getActiveSheet()->getStyle($this->row)->getFont()->setSize($params['font_size']);
-		// bold
-		if (array_key_exists('bold', $params))
-			$this->xls->getActiveSheet()->getStyle($this->row)->getFont()->setBold($params['bold']);
-		// italic
-		if (array_key_exists('italic', $params))
-			$this->xls->getActiveSheet()->getStyle($this->row)->getFont()->setItalic($params['italic']);
-		
-		// set internal params that need to be processed after data are inserted
-		$this->tableParams = array(
-			'header_row' => $this->row,
-			'offset' => $offset,
-			'row_count' => 0,
-			'auto_width' => array(),
-			'filter' => array(),
-			'wrap' => array()
-		);
-		
-		foreach ($data as $d) {
-			// set label
-			$this->xls->getActiveSheet()->setCellValueByColumnAndRow($offset, $this->row, $d['label']);
-			// set width
-			if (array_key_exists('width', $d)) {
-				if ($d['width'] == 'auto')
-					$this->tableParams['auto_width'][] = $offset;
-				else
-					$this->xls->getActiveSheet()->getColumnDimensionByColumn($offset)->setWidth((float)$d['width']);
-			}
-			// filter
-			if (array_key_exists('filter', $d) && $d['filter'])
-				$this->tableParams['filter'][] = $offset;
-			// wrap
-			if (array_key_exists('wrap', $d) && $d['wrap'])
-				$this->tableParams['wrap'][] = $offset;
-			
-			$offset++;
-		}
-		$this->row++;	
-	}
-	
-	/**
-	 * Write array of data to actual row
-	 */
-	public function addTableRow($data) {
-		$offset = $this->tableParams['offset'];
-		
-		foreach ($data as $d) {
-			$this->xls->getActiveSheet()->setCellValueByColumnAndRow($offset++, $this->row, $d);
-		}
-		$this->row++;
-		$this->tableParams['row_count']++;
-	}
-	
-	/**
-	 * End table
-	 * sets params and styles that required data to be inserted
-	 */
-	public function addTableFooter() {
-		// auto width
-		foreach ($this->tableParams['auto_width'] as $col)
-			$this->xls->getActiveSheet()->getColumnDimensionByColumn($col)->setAutoSize(true);
-		// filter (has to be set for whole range)
-		if (count($this->tableParams['filter']))
-			$this->xls->getActiveSheet()->setAutoFilter(PHPExcel_Cell::stringFromColumnIndex($this->tableParams['filter'][0]).($this->tableParams['header_row']).':'.PHPExcel_Cell::stringFromColumnIndex($this->tableParams['filter'][count($this->tableParams['filter']) - 1]).($this->tableParams['header_row'] + $this->tableParams['row_count']));
-		// wrap
-		foreach ($this->tableParams['wrap'] as $col)
-			$this->xls->getActiveSheet()->getStyle(PHPExcel_Cell::stringFromColumnIndex($col).($this->tableParams['header_row'] + 1).':'.PHPExcel_Cell::stringFromColumnIndex($col).($this->tableParams['header_row'] + $this->tableParams['row_count']))->getAlignment()->setWrapText(true);
-	}
-	
-	/**
-	 * Write array of data to actual row starting from column defined by offset
-	 * Offset can be textual or numeric representation
-	 */
-	public function addData($data, $offset = 0) {
-		// solve textual representation
-		if (!is_numeric($offset))
-			$offset = PHPExcel_Cell::columnIndexFromString($offset);
-		
-		foreach ($data as $d) {
-			$this->xls->getActiveSheet()->setCellValueByColumnAndRow($offset++, $this->row, $d);
-		}
-		$this->row++;
-	}
-	
-	/**
-	 * Add worksheet name to worksheet
-	 * 
-	 * @param string $name
-	 */
-	public function addWorksheetName($name) {
-		if(strlen($name) > 0) {
-			$this->xls->getActiveSheet()->setTitle($name);
-		}
-	}
-	
-	/**
-	 * Output file to browser
-	 */
-	public function output($filename = 'export.xlsx') {
-		// set layout
-		$this->_View->layout = '';
-		// headers
-		header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-		header('Content-Disposition: attachment;filename="'.$filename.'"');
-		header('Cache-Control: max-age=0');
-		// writer
-		$objWriter = PHPExcel_IOFactory::createWriter($this->xls, 'Excel2007');
-		$objWriter->save('php://output');
-		// clear memory
-		$this->xls->disconnectWorksheets();
-	}
-	
-	/**
-	 * Load vendor classes
-	 */
-	protected function loadEssentials() {
-		// load vendor class
-		App::import('Vendor', 'PHPExcel/Classes/PHPExcel');
-		if (!class_exists('PHPExcel')) {
-			throw new CakeException('Vendor class PHPExcel not found!');
-		}
-	}
+
+    /**
+     * Create new worksheet from existing file
+     *
+     * @param string $file path to excel file to load
+     * @return $this for method chaining
+     */
+    public function loadWorksheet($file) {
+        // load vendor classes
+        App::import('Vendor', 'PhpExcel.PHPExcel');
+
+        $this->_xls = PHPExcel_IOFactory::load($file);
+        $this->_row = 1;
+
+        return $this;
+    }
+
+    /**
+     * Overloaded __call
+     * Move call to PHPExcel instance
+     *
+     * @param string function name
+     * @param array arguments
+     * @return the return value of the call
+     */
+    public function __call($name, $arguments) {
+        return call_user_func_array(array($this->_xls, $name), $arguments);
+    }
+
+    /**
+     * Set default font
+     *
+     * @param string $name font name
+     * @param int $size font size
+     * @return $this for method chaining
+     */
+    public function setDefaultFont($name, $size) {
+        $this->_xls->getDefaultStyle()->getFont()->setName($name);
+        $this->_xls->getDefaultStyle()->getFont()->setSize($size);
+
+        return $this;
+    }
+
+    /**
+     * Set row pointer
+     *
+     * @param int $row number of row
+     * @return $this for method chaining
+     */
+    public function setRow($row) {
+        $this->_row = (int)$row;
+
+        return $this;
+    }
+
+    /**
+     * Set worksheet name
+     *
+     * @param string $name name
+     * @return $this for method chaining
+     */
+    public function setWorksheetName($name) {
+        $this->_xls->getActiveSheet()->setTitle($name);
+
+        return $this;
+    }
+
+    /**
+     * Start table - insert table header and set table params
+     *
+     * @param array $data data with format:
+     *   label   -   table heading
+     *   width   -   numeric (leave empty for "auto" width)
+     *   filter  -   true to set excel filter for column
+     *   wrap    -   true to wrap text in column
+     * @param array $params table parameters with format:
+     *   offset  -   column offset (numeric or text)
+     *   font    -   font name of the header text
+     *   size    -   font size of the header text
+     *   bold    -   true for bold header text
+     *   italic  -   true for italic header text
+     * @return $this for method chaining
+     */
+    public function setTableHeader($data, $params = array()) {
+        // offset
+        $offset = 0;
+        if (isset($params['offset']))
+            $offset = is_numeric($params['offset']) ? (int)$params['offset'] : PHPExcel_Cell::columnIndexFromString($params['offset']);
+
+        // font name
+        if (isset($params['font']))
+            $this->_xls->getActiveSheet()->getStyle($this->_row)->getFont()->setName($params['font']);
+
+        // font size
+        if (isset($params['size']))
+            $this->_xls->getActiveSheet()->getStyle($this->_row)->getFont()->setSize($params['size']);
+
+        // bold
+        if (isset($params['bold']))
+            $this->_xls->getActiveSheet()->getStyle($this->_row)->getFont()->setBold($params['bold']);
+
+        // italic
+        if (isset($params['italic']))
+            $this->_xls->getActiveSheet()->getStyle($this->_row)->getFont()->setItalic($params['italic']);
+
+        // set internal params that need to be processed after data are inserted
+        $this->_tableParams = array(
+            'header_row' => $this->_row,
+            'offset' => $offset,
+            'row_count' => 0,
+            'auto_width' => array(),
+            'filter' => array(),
+            'wrap' => array()
+        );
+
+        foreach ($data as $d) {
+            // set label
+            $this->_xls->getActiveSheet()->setCellValueByColumnAndRow($offset, $this->_row, $d['label']);
+
+            // set width
+            if (isset($d['width']) && is_numeric($d['width']))
+                $this->_xls->getActiveSheet()->getColumnDimensionByColumn($offset)->setWidth((float)$d['width']);
+            else
+                $this->_tableParams['auto_width'][] = $offset;
+
+            // filter
+            if (isset($d['filter']) && $d['filter'])
+                $this->_tableParams['filter'][] = $offset;
+
+            // wrap
+            if (isset($d['wrap']) && $d['wrap'])
+                $this->_tableParams['wrap'][] = $offset;
+
+            $offset++;
+        }
+        $this->_row++;
+
+        return $this;
+    }
+
+    /**
+     * Write array of data to current row
+     *
+     * @param array $data
+     * @return $this for method chaining
+     */
+    public function addTableRow($data) {
+        $offset = $this->_tableParams['offset'];
+
+        foreach ($data as $d)
+            $this->_xls->getActiveSheet()->setCellValueByColumnAndRow($offset++, $this->_row, $d);
+
+        $this->_row++;
+        $this->_tableParams['row_count']++;
+
+        return $this;
+    }
+
+    /**
+     * End table - set params and styles that required data to be inserted first
+     *
+     * @return $this for method chaining
+     */
+    public function addTableFooter() {
+        // auto width
+        foreach ($this->_tableParams['auto_width'] as $col)
+            $this->_xls->getActiveSheet()->getColumnDimensionByColumn($col)->setAutoSize(true);
+
+        // filter (has to be set for whole range)
+        if (count($this->_tableParams['filter']))
+            $this->_xls->getActiveSheet()->setAutoFilter(PHPExcel_Cell::stringFromColumnIndex($this->_tableParams['filter'][0]) . ($this->_tableParams['header_row']) . ':' . PHPExcel_Cell::stringFromColumnIndex($this->_tableParams['filter'][count($this->_tableParams['filter']) - 1]) . ($this->_tableParams['header_row'] + $this->_tableParams['row_count']));
+
+        // wrap
+        foreach ($this->_tableParams['wrap'] as $col)
+            $this->_xls->getActiveSheet()->getStyle(PHPExcel_Cell::stringFromColumnIndex($col) . ($this->_tableParams['header_row'] + 1) . ':' . PHPExcel_Cell::stringFromColumnIndex($col) . ($this->_tableParams['header_row'] + $this->_tableParams['row_count']))->getAlignment()->setWrapText(true);
+
+        return $this;
+    }
+
+    /**
+     * Write array of data to current row starting from column defined by offset
+     *
+     * @param array $data
+     * @return $this for method chaining
+     */
+    public function addData($data, $offset = 0) {
+        // solve textual representation
+        if (!is_numeric($offset))
+            $offset = PHPExcel_Cell::columnIndexFromString($offset);
+
+        foreach ($data as $d)
+            $this->_xls->getActiveSheet()->setCellValueByColumnAndRow($offset++, $this->_row, $d);
+
+        $this->_row++;
+
+        return $this;
+    }
+
+    /**
+     * Get writer
+     *
+     * @param $writer
+     * @return PHPExcel_Writer_Iwriter
+     */
+    public function getWriter($writer) {
+        return PHPExcel_IOFactory::createWriter($this->_xls, $writer);
+    }
+
+    /**
+     * Save to a file
+     *
+     * @param string $file path to file
+     * @param string $writer
+     * @return bool
+     */
+    public function save($file, $writer = 'Excel2007') {
+        $objWriter = $this->getWriter($writer);
+        return $objWriter->save($file);
+    }
+
+    /**
+     * Output file to browser
+     *
+     * @param string $file path to file
+     * @param string $writer
+     * @return exit on this call
+     */
+    public function output($filename = 'export.xlsx', $writer = 'Excel2007') {
+        // remove all output
+        ob_end_clean();
+
+        // headers
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="' . $filename . '"');
+        header('Cache-Control: max-age=0');
+
+        // writer
+        $objWriter = $this->getWriter($writer);
+        $objWriter->save('php://output');
+
+        exit;
+    }
 }
